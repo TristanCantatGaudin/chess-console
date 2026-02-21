7a1a7936d68b774078e0b1d7d0feac0b6e3ae054
TristanCantatGaudin <tristancantat@gmail.com>
2026-02-21 20:30:42 +0100

    puzzle3 now both follow script

--- src/players/PuzzleOpponent.js
+++ src/players/PuzzleOpponent.js
@@ -1,33 +1,45 @@
 /**
- * Author and copyright: Stefan Haack (https://shaack.com)
- * Repository: https://github.com/shaack/chess-console
- * License: MIT, see file 'LICENSE'
+ * PuzzleOpponent automatically plays the scripted PGN moves on its turn
  */
 
-import {Chess} from "chess.mjs/src/Chess.js"
 import {ChessConsolePlayer} from "../ChessConsolePlayer.js"
+import {Chess} from "chess.mjs/src/Chess.js"
 
-export class RandomPlayer extends ChessConsolePlayer {
+export class PuzzleOpponent extends ChessConsolePlayer {
 
     constructor(chessConsole, name, props = {}) {
         super(chessConsole, name)
-        this.chess = new Chess()
         this.props = {delay: 1000}
         Object.assign(this.props, props)
-    }
 
-    random(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min
+        // Parse PGN into a move queue
+        this.scriptQueue = []
+        if (this.props.scriptPGN) {
+            const tmpChess = new Chess()
+            tmpChess.load_pgn(this.props.scriptPGN)
+            this.scriptQueue = tmpChess.history({verbose: true}) // array of {from, to, promotion, san}
+        }
     }
 
     moveRequest(fen, moveResponse) {
-        setTimeout(() => {
-            this.chess.load(fen)
-            const possibleMoves = this.chess.moves({verbose: true})
-            if (possibleMoves.length > 0) {
-                const randomMove = possibleMoves[this.random(0, possibleMoves.length - 1)]
-                moveResponse({from: randomMove.from, to: randomMove.to, promotion: randomMove.promotion})
-            }
-        }, this.props.delay)
+        const chess = this.chessConsole.state.chess
+        if (chess.gameOver()) return
+
+        // Determine current ply
+        const currentPly = chess.plyCount()
+        const nextMove = this.scriptQueue[currentPly]
+
+        if (nextMove) {
+            console.log(`Ply ${currentPly}: PuzzleOpponent plays ${nextMove.san}`)
+            setTimeout(() => {
+                moveResponse({
+                    from: nextMove.from,
+                    to: nextMove.to,
+                    promotion: nextMove.promotion
+                })
+            }, this.props.delay)
+        } else {
+            console.warn("No scripted move for this ply!")
+        }
     }
 }
